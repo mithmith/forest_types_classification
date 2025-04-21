0c338b09633d8eef6bbd178fd4cc00e67f6b2771
mithmith <ivanivanov77681@gmail.com>
2025-04-21 21:38:31 +0300

    add typing

--- app/dataset/dataset_single.py
+++ app/dataset/dataset_single.py
@@ -455,8 +455,8 @@ class ForestTypesDataset:
                     self.create_forest_mask(sample_id, self.generated_dataset_path, self.forest_model_path)
                 )
             band_data, mask = add_random_rotation_and_flip(band_data, mask)
-            band_data, mask = random_shift(band_data, mask)
             band_data, mask = random_zoom(band_data, mask)
+            band_data, mask = random_shift(band_data, mask)
 
             if not band_data:
                 logger.warning(f"No valid bands found for sample {sample_id}, skipping.")
